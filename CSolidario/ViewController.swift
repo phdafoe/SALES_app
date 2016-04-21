c3e31a95d59b6076470fd64e0feff3255ed9ea76
Andres Ocampo <phdafoe@gmail.com>
2016-04-21 22:44:12 +0200

    Initial Commit

---
+++ CSolidario/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CSolidario
+//
+//  Created by User on 21/4/16.
+//  Copyright © 2016 iCologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
