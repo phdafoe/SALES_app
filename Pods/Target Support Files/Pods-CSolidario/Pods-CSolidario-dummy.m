6ba09f366fdf43fab46a49fe3d4ce8a1a897a090
Andres Ocampo <phdafoe@gmail.com>
2016-04-22 07:11:56 +0200

    Añado pods e imagenes

---
+++ Pods/Target Support Files/Pods-CSolidario/Pods-CSolidario-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CSolidario : NSObject
+@end
+@implementation PodsDummy_Pods_CSolidario
+@end
---
+++ Pods/Target Support Files/Pods-CSolidario/Pods-CSolidario-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CSolidarioVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CSolidarioVersionString[];
+
